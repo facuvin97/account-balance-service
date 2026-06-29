2c1e2e6b4a1d5f8508133613eb8ea465ccdd9c6d
Facu V <facundovinales@hotmail.com>
2026-06-29 11:26:20 -0300

    fix: replace jest.config.ts with jest.config.js to avoid ts-node dep
    Jest requires ts-node to parse a .ts config file. Converting to .js
    (CommonJS) removes that implicit dependency without changing behavior.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
   testMatch: ['<rootDir>/tests/**/*.test.ts'],
 };
 
-export default config;
+module.exports = config;
